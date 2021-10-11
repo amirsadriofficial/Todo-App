e40bce233658a9469685e0faefa995da38dbb573
Amir Sadri <amirsadriofficial@gmail.com>
2021-10-11 08:07:34 +0330

    Update

--- src/app/components/Add-Todo/add-todo.component.ts
+++ src/app/components/Add-Todo/add-todo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TodoService } from 'src/app/services/Todo.service';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'add-todo',
